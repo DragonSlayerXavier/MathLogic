2f02125da748ba686a429eafa2fd7c9b6fc14435
DragonSlayerXavier <amritmahendrajoseph@gmail.com>
2026-03-12 21:56:56 +0530

    Oopsie

--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
 import MathLogic
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println s!"Hello, World!"
---
+++ MathLogic/PropLogic/Deduction/Minimal.lean
@@ -0,0 +1,19 @@
+import MathLogic.PropLogic.Syntax
+
+open Formula
+
+class MinimalDeduction (α : Type) where
+  Pr : Formula α → Prop
+
+  imp_intro : ∀ {p q : Formula α}, (Pr p → Pr q) → Pr (p ⇒ q)
+  imp_el    : ∀ {p q : Formula α}, Pr (p ⇒ q) → Pr p → Pr q
+
+  and_intro : ∀ {p q : Formula α}, Pr p → Pr q → Pr (p ∧ q)
+  and_el_l  : ∀ {p q : Formula α}, Pr (p ∧ q) → Pr p
+  and_el_r  : ∀ {p q : Formula α}, Pr (p ∧ q) → Pr q
+
+  or_intro_l : ∀ {p q : Formula α}, Pr p → Pr (p ∨ q)
+  or_intro_r : ∀ {p q : Formula α}, Pr q → Pr (p ∨ q)
+  or_el      : ∀ {p q r : Formula α}, Pr (p ∨ q) → (Pr p → Pr r) → (Pr q → Pr r) → Pr r
+
+notation "⊢ " p => MinimalDeduction.Pr p
