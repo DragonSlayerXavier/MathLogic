8090a6fb1f90a92f7b146c196e00457302cc1bdb
DragonSlayerXavier <amritmahendrajoseph@gmail.com>
2026-04-02 21:22:30 +0530

    Added equality

--- MathLogic/Peano/Deduction.lean
+++ MathLogic/Peano/Deduction.lean
@@ -4,28 +4,6 @@ import MathLogic.FOL.Deduction.Equality
 
 def V {α : Type} (x : α) : Term PeanoSignature α := .var x
 
-class PeanoDeduction (α : Type) [BEq α] [LawfulBEq α] [ClassicalFOLDeduction PeanoSignature α] where
-  zero_not_succ : ∀ (x : α),
-    ⊢ ¬(S' (V x) ≃ 0)
-
-  succ_inj : ∀ (x y : α),
-    ⊢ (S' (V x) ≃ S' (V y)) ⇒ (V x ≃ V y)
-
-  add_zero : ∀ (x : α),
-    ⊢ (V x + 0) ≃ V x
-
-  add_succ : ∀ (x y : α),
-    ⊢ (V x + S' (V y)) ≃ S' (V x + V y)
-
-  mul_zero : ∀ (x : α),
-    ⊢ (V x * 0) ≃ 0
-
-  mul_succ : ∀ (x y : α),
-    ⊢ (V x * S' (V y)) ≃ (V x * V y) + V x
-
-  induction : ∀ (p : Formula PeanoSignature α) (x : α),
-    ⊢ p⟦x := 0⟧ ⇒ ((∀' x, p ⇒ p⟦x := S' (V x)⟧) ⇒ ∀' x, p)
-
 class PeanoEquality (α : Type) [BEq α] [LawfulBEq α] [MinimalFOLDeduction PeanoSignature α] [EqualityDeduction PeanoSignature α PeanoPred.eq] where
   cong_succ : ∀ (t1 t2 : Term PeanoSignature α),
     ⊢ (t1 ≃ t2) ⇒ (S' t1 ≃ S' t2)
@@ -35,3 +13,25 @@ class PeanoEquality (α : Type) [BEq α] [LawfulBEq α] [MinimalFOLDeduction Pea
 
   cong_mul : ∀ (t1 t2 t3 t4 : Term PeanoSignature α),
     ⊢ (t1 ≃ t2) ⇒ ((t3 ≃ t4) ⇒ (t1 * t3 ≃ t2 * t4))
+
+class PeanoDeduction (α : Type) [BEq α] [LawfulBEq α] [ClassicalFOLDeduction PeanoSignature α] where
+  zero_not_succ : ∀ (t : Term PeanoSignature α),
+    ⊢ ¬(S' t ≃ 0)
+
+  succ_inj : ∀ (t1 t2 : Term PeanoSignature α),
+    ⊢ (S' t1 ≃ S' t2) ⇒ (t1 ≃ t2)
+
+  add_zero : ∀ (t : Term PeanoSignature α),
+    ⊢ (t + 0) ≃ t
+
+  add_succ : ∀ (t1 t2 : Term PeanoSignature α),
+    ⊢ (t1 + S' t2) ≃ S' (t1 + t2)
+
+  mul_zero : ∀ (t : Term PeanoSignature α),
+    ⊢ (t * 0) ≃ 0
+
+  mul_succ : ∀ (t1 t2 : Term PeanoSignature α),
+    ⊢ (t1 * S' t2) ≃ (t1 * t2) + t1
+
+  induction : ∀ (p : Formula PeanoSignature α) (x : α),
+    ⊢ p⟦x := 0⟧ ⇒ ((∀' x, p ⇒ p⟦x := S' (V x)⟧) ⇒ ∀' x, p)
